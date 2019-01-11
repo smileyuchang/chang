eb0d70bf13d9b30716ecbc36e07ef97c8ef8e5fc
1258874300@qq.com <1258874300@qq.com>
2019-01-11 17:35:12 +0800

    rabbitMQ延时消息

--- renren-api/src/main/java/io/renren/test/rabbitmq/Sender.java
+++ renren-api/src/main/java/io/renren/test/rabbitmq/Sender.java
@@ -16,11 +16,15 @@ public class Sender {
 
     public void sendDirectQueue() {
         User user = new User();
-        user.setUserId("123456");
-        user.setName("lizhencheng");
+        user.setUserId("世界的看法就是的");
+        user.setName("想，支持，下周末，这些吗吗存在每次v");
         log.info("【sendDirectQueue已发送消息】");
          // 第一个参数是指要发送到哪个队列里面， 第二个参数是指要发送的内容
-        this.amqpTemplate.convertAndSend(RabbitMQConfig.QUEUE, user);
+        //this.amqpTemplate.convertAndSend(RabbitMQConfig.QUEUE, user);
+        this.amqpTemplate.convertAndSend( RabbitMQConfig.QUEUE,user,message -> {
+            message.getMessageProperties().setExpiration( 1000 * 60 + "");
+            return message;
+        });
     }
 
     public void sendTopic() {
@@ -48,7 +52,11 @@ public class Sender {
         // 注意， 这里的第2个参数为空。
         // 因为fanout 交换器不处理路由键，只是简单的将队列绑定到交换器上，
         // 每个发送到交换器的消息都会被转发到与该交换器绑定的所有队列上
-        this.amqpTemplate.convertAndSend(RabbitMQConfig.FANOUT_EXCHANGE, "", user );
+        //this.amqpTemplate.convertAndSend(RabbitMQConfig.FANOUT_EXCHANGE, "", user );
+        this.amqpTemplate.convertAndSend( RabbitMQConfig.FANOUT_EXCHANGE,user,message -> {
+            message.getMessageProperties().setExpiration( 1000 * 60 + "");
+            return message;
+        });
     }
 
     public void sendDelay() {
