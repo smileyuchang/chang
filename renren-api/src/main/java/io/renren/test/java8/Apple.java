a4100b6b6f64396d73943b4f6df6fc2ac7a594e9
1258874300@qq.com <1258874300@qq.com>
2019-07-31 18:01:59 +0800

    java8默认方法，函数式接口，lambda表达式，streams的应用

---
+++ renren-api/src/main/java/io/renren/test/java8/Apple.java
@@ -0,0 +1,25 @@
+package io.renren.test.java8;
+
+import lombok.Data;
+
+@Data
+public class Apple {
+    /** 编号 */
+    private long id;
+
+
+    /** 重量 */
+    private float weight;
+
+    /** 产地 */
+    private String origin;
+
+    public Apple() {
+    }
+
+    public Apple(long id, float weight, String origin) {
+        this.id = id;
+        this.weight = weight;
+        this.origin = origin;
+    }
+}
