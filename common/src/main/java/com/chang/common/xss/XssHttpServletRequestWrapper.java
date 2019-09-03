7bb2506333fd45101552333388d1e81b33083b85
1258874300@qq.com <1258874300@qq.com>
2019-09-03 15:06:44 +0800

    删了些没用的东西

--- common/src/main/java/com/chang/common/xss/XssHttpServletRequestWrapper.java
+++ common/src/main/java/com/chang/common/xss/XssHttpServletRequestWrapper.java
@@ -1,21 +1,4 @@
-/**
- * Copyright 2018 人人开源 http://www.renren.io
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package com.chang.common.xss;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpHeaders;
@@ -32,8 +15,6 @@ import java.util.Map;
 
 /**
  * XSS过滤处理
- * @author chenshun
- * @email sunlightcs@gmail.com
  * @date 2017-04-01 11:29
  */
 public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
--- server/src/main/java/com/chang/modules/job/task/TestTask.java
+++ server/src/main/java/com/chang/modules/job/task/TestTask.java
@@ -1,21 +1,5 @@
 package com.chang.modules.job.task;
 
-/**
- * Copyright 2018 人人开源 http://www.renren.io
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 import com.chang.modules.sys.entity.SysUserEntity;
 import com.chang.modules.sys.service.SysUserService;
 import org.apache.commons.lang.builder.ToStringBuilder;
@@ -28,8 +12,6 @@ import org.springframework.stereotype.Component;
  * 测试定时任务(演示Demo，可删除)
  *
  * testTask为spring bean的名称
- *
- * @author Mark sunlightcs@gmail.com
  * @since 1.2.0 2016-11-28
  */
 @Component("testTask")
